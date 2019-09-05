178cb1086c73d372b3dae289ea6a5e13e3554c24
Sean Doyle <sdoylelambda@gmail.com>
2019-09-05 13:16:46 -0400

    Backend added.

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const secrets = require('../config/secrets');
+
+console.log('environment', process.env.NODE_ENV)
 
 const apiRouter = require('./api-router.js');
 

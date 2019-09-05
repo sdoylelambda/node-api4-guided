833e75e07a32762c081a7796d5a3c84c5a5e3a1c
Sean Doyle <sdoylelambda@gmail.com>
2019-09-05 13:56:37 -0400

    Duplicate removed.

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,25 @@ module.exports = {
     connection: {
       filename: './data/shouts.db3',
     },
-
-    production: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './data/shouts.db3',
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
-    
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+​
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/shouts.db3',
+    },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
